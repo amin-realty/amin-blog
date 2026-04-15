d173f0a44fdbdb075ec17e6c3fca0c4697153b79
amin-realty <amin-realty@users.noreply.github.com>
2026-04-15 19:52:59 +0800

    Disable cache to force fresh OG metadata on blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
   },
 }
 
-export const revalidate = 3600
+export const revalidate = 0
 
 export default async function BlogPage() {
   const posts = await getAllPosts()
